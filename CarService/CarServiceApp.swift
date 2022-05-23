3b50c347ae681fe5359099bd4c9327c3b560ee24
Harutyun Shamyan <shamyan.harut@gmail.com>
2022-05-23 17:43:30 +0400

    Initial Commit

---
+++ CarService/CarServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarServiceApp.swift
+//  CarService
+//
+//  Created by Harutyun Shamyan on 23.05.22.
+//
+
+import SwiftUI
+
+@main
+struct CarServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarService
+//
+//  Created by Harutyun Shamyan on 23.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
